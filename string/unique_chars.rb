d7207271e8a2357c5e729383b2e6ed8e29967be1
Dan <baruahd@tcd.ie>
2019-10-26 14:47:43 +0530

    Check if a string has unique character solution

---
+++ string/unique_chars.rb
@@ -0,0 +1,25 @@
+=begin
+  Implement an algorithm to determine if a sting has all unique characters.
+=end
+
+require 'test/unit/assertions'
+include Test::Unit::Assertions
+
+def is_unique_chars(str)
+  # assuming the provided string is an ASCII string.
+  return false if str.empty?
+  char_set = []
+  str.each_byte do |c|
+    ascii_val = c
+    if(char_set[ascii_val])
+      return false
+    end
+    char_set[ascii_val] = true
+  end
+  true
+end
+
+assert(is_unique_chars('str'))
+assert(!is_unique_chars('abcbca'))
+assert(!is_unique_chars('strstrstr'))
+assert(!is_unique_chars(''))
