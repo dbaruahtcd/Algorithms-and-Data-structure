674736ab9e3d0b1879488513eb8559dd1a39826c
Dan <baruahd@tcd.ie>
2020-03-08 12:16:55 +0000

    Updated specs for binary heap implementation

--- priority_queue/binary_heap.rb
+++ priority_queue/binary_heap.rb
@@ -2,9 +2,11 @@
 # top of the tree and that the childrens keys are not greater than that of the
 # parent
 # If the parent key is at node with index i then the children will be at node 2i & 2i + 1
+require_relative '../spec_helper'
 
 class BinaryHeap
   attr_reader :heap, :size
+
   def initialize
     @heap = []
     @size = 0
@@ -20,9 +22,9 @@ class BinaryHeap
   def delete_max
     max_element = @heap[1]
     exchange(1, @size)
-    sink(1)
     @heap[@size] = nil
     @size -= 1
+    sink(1)
     max_element
   end
 
@@ -34,12 +36,15 @@ class BinaryHeap
     end
   end
 
+  # Dont forget to perform two checks here, one for parent and child and one
+  # for both children
   def sink(index)
-    while(index <= @size)
+    while(2*index <= @size)
       k = index * 2
-      k += 1 if (less(k, k+1) && (k < @size))
+      k += 1 if ((k < @size) && less(k, k+1))
+      break if(!less(index, k))
       exchange(index, k)
-      index *= 2
+      index = k
     end
   end
 
@@ -57,17 +62,12 @@ end
 
 heap = BinaryHeap.new
 (1..5).to_a.each { |i| heap.insert(i) }
-# heap.insert(1)
-# heap.insert(2)
-# heap.insert(3)
-# heap.insert(4)
-# heap.insert(5)
-puts heap.heap
-puts heap.size
-puts "deleted : #{heap.delete_max}"
-puts "deleted : #{heap.delete_max}"
-puts "deleted : #{heap.delete_max}"
-puts "deleted : #{heap.delete_max}"
-puts "deleted : #{heap.delete_max}"
-# puts heap.size
-# puts heap.heap
+
+assert_equal([nil, 5, 4, 2, 1, 3], heap.heap)
+assert_equal(5, heap.size)
+assert_equal(5, heap.delete_max)
+
+assert_equal(4, heap.delete_max)
+assert_equal([nil, 3, 1, 2, nil, nil], heap.heap)
+
+assert_equal(3, heap.size)
