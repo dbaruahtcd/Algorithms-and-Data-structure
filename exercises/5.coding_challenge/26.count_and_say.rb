2d450228fc97b7fdd8e0c903b137dba2ae144de9
Dan <baruahd@tcd.ie>
2020-07-28 22:27:24 +0100

    added solution for count and say

--- exercises/5.coding_challenge/26.count_and_say.rb
+++ exercises/5.coding_challenge/26.count_and_say.rb
@@ -31,3 +31,73 @@ Explanation: For n = 3 the term was "21" in which we have two groups "2" and "1"
 be read as "12" which means frequency = 1 and value = 2, the same way "1" is read as "11", 
 so the answer is the concatenation of "12" and "11" which is "1211".
 =end
+require 'byebug'
+def count_and_say(n)
+  
+  return '1' if n == 1
+  return '11' if n == 2
+  curr_str = '11'
+  j = 3
+  while j <= n
+    debugger
+    str_arr = curr_str.split('')
+    str_at_index = ''
+    for i in 0...(str_arr.length-1) do
+      cnt = 1
+      x = str_arr[i]
+      if x == str_arr[i+1] && ((i+1) == (str_arr.length-1))
+        cnt += 1
+        str_at_index += "#{cnt}#{x}"
+      elsif x == str_arr[i+1]
+        cnt += 1
+      elsif x != str_arr[i+1]
+        str_at_index += "#{cnt}#{x}"
+        # x = str_arr[i+1]
+        # cnt = 1
+      end
+    end
+    curr_str = str_at_index
+    j += 1
+  end
+  curr_str
+end
+
+def count_and_say_imp(n)
+  next_digit = '1'
+  return next_digit if n == 1
+  j = 2
+  prev_digit = ''
+  cnt = 0
+  while(j <= n)
+    arr = next_digit.split('')
+    arr.each_with_index do |d, i|
+      if prev_digit.empty?
+        prev_digit = d
+        cnt += 1
+        next
+      end
+      if prev_digit == d
+        cnt += 1
+      else
+        next_digit = "#{cnt}#{prev_digit}"
+        prev_digit = d
+        cnt = 1
+        next_digit += "#{cnt}#{prev_digit}" if i == arr.length - 1
+        return next_digit
+      end
+    end
+    next_digit = "#{cnt}#{prev_digit}"
+    cnt = 0
+    prev_digit = ''
+    j += 1
+  end
+  next_digit
+end
+
+# puts count_and_say_imp(1)
+# puts count_and_say_imp(2)
+# puts count_and_say_imp(3)
+# puts count_and_say_imp(4) # 1211
+puts count_and_say_imp(5) #"111221"
+# puts count_and_say(3)
+# puts count_and_say_imp(4)
