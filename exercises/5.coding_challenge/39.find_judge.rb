c9e7266954c654ede4bf3382ea6a5b6ca81d3377
Dan <baruahd@tcd.ie>
2021-01-08 10:49:33 +0000

    Added solution for find judge through using indegree and outdegree

---
+++ exercises/5.coding_challenge/39.find_judge.rb
@@ -0,0 +1,72 @@
+=begin
+In a town, there are N people labelled from 1 to N.  There is a rumor that one of these people is secretly the town judge.
+
+If the town judge exists, then:
+
+The town judge trusts nobody.
+Everybody (except for the town judge) trusts the town judge.
+There is exactly one person that satisfies properties 1 and 2.
+You are given trust, an array of pairs trust[i] = [a, b] representing that the person labelled a trusts the person labelled b.
+
+If the town judge exists and can be identified, return the label of the town judge.  Otherwise, return -1.
+
+ 
+
+Example 1:
+
+Input: N = 2, trust = [[1,2]]
+Output: 2
+Example 2:
+
+Input: N = 3, trust = [[1,3],[2,3]]
+Output: 3
+Example 3:
+
+Input: N = 3, trust = [[1,3],[2,3],[3,1]]
+Output: -1
+Example 4:
+
+Input: N = 3, trust = [[1,2],[2,3]]
+Output: -1
+Example 5:
+
+Input: N = 4, trust = [[1,3],[1,4],[2,3],[2,4],[4,3]]
+Output: 3
+ 
+
+Constraints:
+
+1 <= N <= 1000
+0 <= trust.length <= 10^4
+trust[i].length == 2
+trust[i] are all different
+trust[i][0] != trust[i][1]
+1 <= trust[i][0], trust[i][1] <= N
+=end
+require '../../spec_helper'
+# Solution 1. Making use of in degree and outdegree to find the judge
+
+def find_judge(n, trust)
+  return -1 if trust.length < n - 1 
+  
+  indegree = Array.new(n+1, 0)
+  outdegree = Array.new(n+1, 0)
+  trust.each do |r|
+    outdegree[r[0]] += 1
+    indegree[r[1]] += 1
+  end
+  
+  (0...n).each do |i|
+    return i if (outdegree[i] == 0 && indegree[i] == (n - 1)) 
+  end
+  -1 
+end
+
+n = 3
+trust = [[1,3],[2,3],[3,1]]
+
+n1 = 4
+trust1 = [[1,3],[1,4],[2,3],[2,4],[4,3]]
+
+assert_equal(find_judge(n, trust), -1)
+assert_equal(find_judge(n1, trust1), 3)
