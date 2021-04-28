164a337d85352a51f5a123f73f0d050a6b5bd1a6
dbaruah <baruahd@tcd.ie>
2021-04-28 19:22:17 +0100

    Added solution for truncating string

---
+++ exercises/5.coding_challenge/55.truncate_sentence.rb
@@ -0,0 +1,37 @@
+=begin
+A sentence is a list of words that are separated by a single space with no leading or trailing spaces.
+Each of the words consists of only uppercase and lowercase English letters (no punctuation).
+
+For example, "Hello World", "HELLO", and "hello world hello world" are all sentences.
+You are given a sentence s​​​​​​ and an integer k​​​​​​. You want to truncate s​​​​​​ such that it contains only the
+first k​​​​​​ words. Return s​​​​​​ after truncating it.
+
+Example 1:
+
+Input: s = "Hello how are you Contestant", k = 4
+Output: "Hello how are you"
+Explanation:
+The words in s are ["Hello", "how" "are", "you", "Contestant"].
+The first 4 words are ["Hello", "how", "are", "you"].
+Hence, you should return "Hello how are you".
+Example 2:
+
+Input: s = "What is the solution to this problem", k = 4
+Output: "What is the solution"
+Explanation:
+The words in s are ["What", "is" "the", "solution", "to", "this", "problem"].
+The first 4 words are ["What", "is", "the", "solution"].
+Hence, you should return "What is the solution".
+=end
+
+require '../../spec_helper'
+
+def trucate_sentences(s, k)
+  s.split(' ')[0...k].join(' ')
+end
+
+s = "Hello how are you Contestant"
+k = 4
+
+assert_equal(trucate_sentences(s, k), 'Hello how are you')
+
