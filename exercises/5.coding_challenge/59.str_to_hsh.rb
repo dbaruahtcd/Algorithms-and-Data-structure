9c557238dbd4f590ae431ed4a7be1afdd7fdcce4
dbaruah <baruahd@tcd.ie>
2021-04-29 20:45:17 +0100

    Added solution for converting from string to hsh

---
+++ exercises/5.coding_challenge/59.str_to_hsh.rb
@@ -0,0 +1,25 @@
+=begin
+Please write a function that will take a string as input and return a hash. The string will be formatted as such. The key will always be a symbol and the value will always be an integer.
+
+"a=1, b=2, c=3, d=4"
+This string should return a hash that looks like
+
+{ :a => 1, :b => 2, :c => 3, :d => 4}
+=end
+require '../../spec_helper'
+
+def str_to_hash(str)
+  hsh = {}
+  return hsh if str.empty?
+  str = str.split(',').map(&:strip)
+  str.each do |s|
+    key = s[0].to_sym
+    val = s[2].to_i
+    hsh[key] = val
+  end
+  hsh
+end
+
+# puts str_to_hash("a=1, b=2, c=3, d=4")
+
+assert_equal(str_to_hash("a=1, b=2, c=3, d=4"),{ :a => 1, :b => 2, :c => 3, :d => 4})
